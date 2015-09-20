c229564eb18385c30ca8b5fc145bca025417d51a
stupiding <guojinma@gmail.com>
2015-09-20 10:35:23 +0000

    modify README.md

--- read_data.py
+++ read_data.py
@@ -30,3 +30,29 @@ for sub in np.arange(n_subs):
     label.append(sub_label)
 
 np.save('eeg_train.npy', [data, label])
+
+csvdir = 'data/test'
+n_subs = 12
+n_series = 2
+n_channels = 32
+
+data = []
+label = []
+
+for sub in np.arange(n_subs):
+    sub_data = []
+    sub_label = []
+    for series in np.arange(9, 9 + n_series):
+        csv = 'subj' + str(sub + 1) + '_series' + str(series + 1) + '_data.csv'
+        series_data = pd.read_csv(os.path.join(csvdir, csv))
+        ch_names = list(series_data.columns[1:])
+        series_data = np.array(series_data[ch_names], 'float32')
+        sub_data.append(series_data)
+
+        series_label = np.zeros([series_data.shape[0], 6])
+        sub_label.append(series_label)
+
+    data.append(sub_data)
+    label.append(sub_label)
+
+np.save('eeg_test.npy', [data, label])
